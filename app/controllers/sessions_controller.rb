0349fa8cdf3d3ebd0486b28c780a03faf02b9095
MOBOLA MORONTO <t2wycemo@gmail.com>
2020-07-12 10:00:13 +0000

    Automatically backed up by Learn

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -3,12 +3,18 @@ class SessionsController < ApplicationController
   end
   
   def create
-    session[:name] = params[:name]
-    redirect_to '/'
+    if params[:name].blank?
+      redirect_to login_path
+      
+    else  
+      session[:name] = params[:name]
+      redirect_to root_path
+    end  
   end
   
   def destroy
     session.delete :name
+    redirect_to root_path
   end
   
 end
